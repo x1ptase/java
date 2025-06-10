46e0f377f1cc386246dcf36d35ebb4088f41a6ea
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-06-10 22:07:07 +0700

    update src

---
+++ [LAB211] OOP with Java Lab/RoomMngPrj/RoomMngPrj-main/src/RoomOrderManager.java
@@ -0,0 +1,86 @@
+
+import tool.ConsoleInputter;
+import core.*;
+import java.util.Calendar;
+
+public class RoomOrderManager {
+
+    public static void main(String[] args) {
+        RoomList rList = new RoomList();
+        GuestList gList = new GuestList();
+
+        rList.readFile(RoomList.fName);
+        gList.readFromFile(GuestList.fname);
+        int choice;
+        boolean changed = false;
+        boolean isSaved = false;
+        do {
+            choice = ConsoleInputter.intMenu(
+                    "Display Available Room List",
+                    "Enter Guest Information",
+                    "Update Guest Stay Information",
+                    "Search Guest by National ID",
+                    "Delete Guest Reservation Before Arrival",
+                    "Display All Guest",
+                    "List Vacant Rooms",
+                    "Monthly Revenue Report",
+                    "Revenue Report by Room Type",
+                    "Save Guest Information",
+                    "Exit");
+            if (choice >= 2 && choice <= 5) {
+                changed = true;
+            }
+            switch (choice) {
+                case 1://Ok
+                    rList.displayAll();
+                    ConsoleInputter.pause();
+                    break;
+                case 2:
+                    gList.addGuest();//xong
+                    ConsoleInputter.pause();
+                    break;
+                case 3:
+                    gList.updateGuest();//Xong
+                    ConsoleInputter.pause();
+                    break;
+                case 4://xong
+                    gList.seacrchByID();
+                    ConsoleInputter.pause();
+                    break;
+                case 6://xong
+                    gList.displayAll();
+                    ConsoleInputter.pause();
+                    break;
+                case 5://xong
+                    gList.deleteGuest();
+                    ConsoleInputter.pause();
+                    break;
+                case 7://xong
+                    rList.vacantRoomList(gList).displayAll();
+                    ConsoleInputter.pause();
+                    break;
+                case 8://xong
+                    rList.monthlyReport(gList);
+                    ConsoleInputter.pause();
+                    break;
+                case 9://xong
+                    rList.revenueReport(gList);
+                    ConsoleInputter.pause();
+                    break;
+                case 10://xong
+                    gList.saveFile(GuestList.fname);
+                    ConsoleInputter.pause();
+                    isSaved = true;
+                    break;
+                case 11:
+                    if (changed && !isSaved) {
+                        boolean resp = tool.ConsoleInputter.getBoolean("Data changed. Save or not");
+                        if (resp) {
+                            gList.saveFile(GuestList.fname);
+                        }
+                    }
+            }
+
+        } while (choice != 11);
+    }
+}
---
+++ [LAB211] OOP with Java Lab/RoomMngPrj/RoomMngPrj-main/src/core/Room.java
@@ -0,0 +1,141 @@
+package core;
+
+import java.util.Calendar;
+import java.util.Date;
+import tool.ConsoleInputter;
+
+public class Room {
+
+    private String roomID;
+    private String roomName;
+    private String roomType;
+    private float dailyRate;
+    private float capacity;
+    private String funrnitureDescription;
+
+    public Room() {
+    }
+
+    public Room(String roomID) {
+        this.roomID = roomID;
+    }
+
+    public Room(String roomID, String roomName, String roomType, float dailyRate, float capacity, String funrnitureDescription) {
+        this.roomID = roomID;
+        this.roomName = roomName;
+        this.roomType = roomType;
+        this.dailyRate = dailyRate;
+        this.capacity = capacity;
+        this.funrnitureDescription = funrnitureDescription;
+    }
+
+    public String getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(String roomID) {
+        this.roomID = roomID;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public float getDailyRate() {
+        return dailyRate;
+    }
+
+    public void setDailyRate(float dailyRate) {
+        this.dailyRate = dailyRate;
+    }
+
+    public float getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(float capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getFunrnitureDescription() {
+        return funrnitureDescription;
+    }
+
+    public void setFunrnitureDescription(String funrnitureDescription) {
+        this.funrnitureDescription = funrnitureDescription;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Room room = (Room) obj;
+        return this.getRoomID().equalsIgnoreCase(room.getRoomID());
+    }
+
+    public boolean isRented(GuestList list, Date sDate, int rentals) {
+        if (list.isEmpty()) {
+            return false;
+        }
+
+        for (Guest guest : list) {
+            if (guest.getDesiredRID().trim().equalsIgnoreCase(this.roomID.trim())) {
+                Date startDate = guest.getStartDate();
+                int duration = guest.getRentalDate();
+
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(startDate);
+                cal.add(Calendar.DATE, duration - 1);
+                Date endDate = cal.getTime();
+
+                for (int i = 0; i < rentals; i++) {
+                    Calendar checkCal = Calendar.getInstance();
+                    checkCal.setTime(sDate);
+                    checkCal.add(Calendar.DATE, i);
+                    Date checkDate = checkCal.getTime();
+                    if (ConsoleInputter.isSameDay(checkDate, startDate)) {
+                        return true;
+                    }
+                    if (!checkDate.before(startDate) && !checkDate.after(endDate)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isRented(GuestList list, Date sDate) {
+        if (list.isEmpty()) {
+            return false;
+        }
+
+        for (Guest guest : list) {
+            if (guest.getDesiredRID().trim().equalsIgnoreCase(this.roomID.trim())) {
+                Date startDate = guest.getStartDate();
+                int duration = guest.getRentalDate();
+
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(startDate);
+                cal.add(Calendar.DATE, duration - 1);
+                Date endDate = cal.getTime();
+
+                if (!sDate.before(startDate) && !sDate.after(endDate)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}//Room
