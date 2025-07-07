288d8bfce4ab4ba42d1870af227e84b147c8fb34
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-07-07 22:45:24 +0700

    update src

--- [LAB211]OOPwithJavaLab/Base1/demoLoops/src/LoopWhile.java
+++ [LAB211]OOPwithJavaLab/Base1/demoLoops/src/LoopWhile.java
@@ -1,7 +1,7 @@
 public class LoopWhile{
     public static void main(String[] args){
         int i=0;
-        while(i<100){
+        while(i < 100){
             System.out.println((i+1) + ". I love you");
             i++;
         }
