f1be9ce1d0b8d6832e80f3486fe7d8ec0cc3d11e
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-07-07 22:45:45 +0700

    update src

--- [LAB211]OOPwithJavaLab/Base1/demoLoops/src/InfiniteWhile.java
+++ [LAB211]OOPwithJavaLab/Base1/demoLoops/src/InfiniteWhile.java
@@ -2,7 +2,7 @@ public class InfiniteWhile{
     public static void main(String[] args){
         int i=0;
         while(true){
-            System.out.println((i+1) + ". I love you");
+            System.out.println((i + 1) + ". I love you");
             i++;
         }
     }
--- [LAB211]OOPwithJavaLab/Base1/demoLoops/src/LoopFor.java
+++ [LAB211]OOPwithJavaLab/Base1/demoLoops/src/LoopFor.java
@@ -1,6 +1,6 @@
 public class LoopFor{
     public static void main(String[] args){
         for(int i=0; i<100; i++)
-            System.out.println((i+1) + ". I love you");
+            System.out.println((i + 1) + ". I love you");
     } 
 }
