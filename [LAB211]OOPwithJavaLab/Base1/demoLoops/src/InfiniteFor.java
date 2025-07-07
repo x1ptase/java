faf93ac888c140b44df744a89f421e50ee935071
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-07-07 22:46:07 +0700

    update src

--- [LAB211]OOPwithJavaLab/Base1/demoLoops/src/InfiniteFor.java
+++ [LAB211]OOPwithJavaLab/Base1/demoLoops/src/InfiniteFor.java
@@ -1,6 +1,6 @@
 public class InfiniteFor{
     public static void main(String[] args){
         for(int i=0; ;i++)
-            System.out.println((i+1) + ". I love you");
+            System.out.println((i + 1) + ". I love you");
     }
 }
