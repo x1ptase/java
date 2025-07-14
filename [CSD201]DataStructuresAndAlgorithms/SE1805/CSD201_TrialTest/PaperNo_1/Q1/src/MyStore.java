ae326ea8bb91f704b8ce0a3b3a1fd2ff02988c50
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-07-14 20:47:00 +0700

    update src

---
+++ [CSD201]DataStructuresAndAlgorithms/SE1805/CSD201_TrialTest/PaperNo_1/Q1/src/MyStore.java
@@ -0,0 +1,230 @@
+/* This program contains 2 parts: (1) and (2)
+   YOUR TASK IS TO COMPLETE THE PART  (2)  ONLY
+ */
+//(1)==============================================================
+import java.util.*;
+import java.io.*;
+
+class FruitList {
+
+    Node head, tail;
+
+    FruitList() {
+        head = tail = null;
+    }
+
+    boolean isEmpty() {
+        return (head == null);
+    }
+
+    void clear() {
+        head = tail = null;
+    }
+
+    void loadDataFruit(int k) //do not edit this function
+    {
+        String[] a = Lib.readLineToStrArray("data.txt", k);
+        int[] b = Lib.readLineToIntArray("data.txt", k + 1);
+        int[] c = Lib.readLineToIntArray("data.txt", k + 2);
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            addLast(a[i], b[i], c[i]);
+        }
+    }
+
+    void addLast(String type, int amount, int price) {
+        //You should write here appropriate statements to complete this function.
+        //--------------------------------------------------------
+        Node p=new Node(new Fruit(type, amount, price));
+        if(isEmpty()){
+            head=tail=p;
+        } else{
+            tail.next=p;
+            tail=p;
+        }
+        //---------------------------------------------------------
+    }
+
+}
+
+class RequestQueue {
+
+    Node front, rear;
+
+    RequestQueue() {
+        front = rear = null;
+    }
+
+    boolean isEmpty() {
+        return (front == null);
+    }
+
+    void clear() {
+        front = rear = null;
+    }
+
+    void loadDataRequest(int k) //do not edit this function
+    {
+        String[] a = Lib.readLineToStrArray("data.txt", k + 3);
+        int[] b = Lib.readLineToIntArray("data.txt", k + 4);
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            enQueue(a[i], b[i]);
+        }
+    }
+
+    void enQueue(String type, int amount) {
+        //You should write here appropriate statements to complete this function.
+        //--------------------------------------------------------
+        Node p=new Node(new Fruit(type, amount));
+        if(isEmpty()){
+            front=rear=p;
+        } else{
+            rear.next=p;
+            rear=p;
+        }
+        //---------------------------------------------------------
+    }
+
+    Fruit deQueue() {
+        Fruit tmp = new Fruit();
+        //You should write here appropriate statements to complete this function.
+        //--------------------------------------------------------
+        if(isEmpty()) return null;
+        tmp=front.info;
+        front=front.next;
+        if(front == null) rear=null;
+        //---------------------------------------------------------
+        return tmp;
+    }
+    
+}
+
+class MyStore {
+
+    FruitList FList = new FruitList();
+    RequestQueue RQueue = new RequestQueue();
+
+    MyStore() {
+    }
+
+    void fvisit(Node p, RandomAccessFile f) throws Exception {
+        if (p != null) {
+            f.writeBytes(p.info + " ");
+        }
+    }
+
+    void ftraverse(RandomAccessFile f) throws Exception {
+        Node p = FList.head;
+        f.writeBytes("Data Fruit: ");
+        if (p == null) {
+            f.writeBytes("Empty");
+        }
+        while (p != null) {
+            fvisit(p, f); // You will use this statement to write information of the node p to the file
+            p = p.next;
+        }
+        f.writeBytes("\r\n");
+        f.writeBytes("Request   : ");
+        p = RQueue.front;
+        if (p == null) {
+            f.writeBytes("Empty");
+        }
+        while (p != null) {
+            f.writeBytes("(" + p.info.getType() + "," + p.info.getAmount() + ") ");
+            p = p.next;
+        }
+        f.writeBytes("\r\n");
+    }
+
+    void load(int k) throws Exception {
+        FList.loadDataFruit(k);
+        RQueue.loadDataRequest(k);
+    }
+
+    void f1() throws Exception {
+        load(1);
+        String fname = "f1.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        ftraverse(f);
+
+        f.close();
+    }
+
+    void f2() throws Exception {
+        load(1);
+        String fname = "f2.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        ftraverse(f);
+        //You should write here appropriate statements to complete this function.
+        //--------------------------------------------------------
+        Fruit req=RQueue.deQueue();
+        if(req != null){
+            purchase(req);
+        }
+        //---------------------------------------------------------
+        ftraverse(f);
+        f.close();
+    }
+
+    void f3() throws Exception {
+        load(1);
+        String fname = "f3.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        ftraverse(f);
+        //You should write here appropriate statements to complete this function.
+        //--------------------------------------------------------
+        
+        //---------------------------------------------------------
+        ftraverse(f);
+        f.close();
+    }
+
+    void f4() throws Exception {
+        load(1);
+        String fname = "f4.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        ftraverse(f);
+        int S = 0;
+        //You should write here appropriate statements to complete this function.
+        //--------------------------------------------------------
+
+        //---------------------------------------------------------
+        f.writeBytes("Money     : " + S + " ");
+        f.close();
+    }
+    
+    public int purchase(Fruit request){
+        Node p=FList.head;
+        while(p != null){
+            if(p.info.getType().equals(request.getType())){
+                if(!p.info.getType().equals("M") && !request.getType().equals("F")){
+                    int purchaseAmount=Math.min(request.getAmount(), p.info.getAmount());
+                    if(purchaseAmount > 0){
+                        p.info.setAmount(p.info.getAmount() - purchaseAmount);
+                        return purchaseAmount * p.info.getPrice();
+                    }
+                }
+                break;
+            }
+            p=p.next;
+        }
+        return 0;
+    }
+}
