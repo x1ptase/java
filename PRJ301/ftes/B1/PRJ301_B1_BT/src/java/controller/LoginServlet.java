dd2047a858be3299dcdd7e081ab1cfbc648a7b6e
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-10-20 22:46:40 +0700

    update src

---
+++ PRJ301/ftes/B1/PRJ301_B1_BT/src/java/controller/LoginServlet.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author x1pta
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/process"})
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out=response.getWriter();
+        String userName=request.getParameter("txtUsername");
+        String password=request.getParameter("txtPassword");
+        
+        if(userName.equals("Admin") && password.equals("123")){
+            out.println("Login Successfully!!!");
+        } else{
+            out.println("Login Failed!!");
+        }
+    }
+}
