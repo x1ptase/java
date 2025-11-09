cb134b6826a238ca1aff32c828a40c8ae9933051
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-11-09 23:45:09 +0700

    update src

--- PRJ301/PE_HCM/SU25/PE_PRJ301_SU25_296710/PE_PRJ301_SU25_296710/PaperNo_1/1/PE_Prj301/src/java/pe/model/RoomForRentDto.java
+++ PRJ301/PE_HCM/SU25/PE_PRJ301_SU25_296710/PE_PRJ301_SU25_296710/PaperNo_1/1/PE_Prj301/src/java/pe/model/RoomForRentDto.java
@@ -11,5 +11,92 @@ package pe.model;
  */
 public class RoomForRentDto {
     //-----            your code here   --------------------------------
+    private int id;
+    private String title;
+    private float price;
+    private String location;
+    private String description;
+    private String postedDate;
+    private int status;
+    private String username;
+
+    public RoomForRentDto() {
+    }
+
+    public RoomForRentDto(int id, String title, float price, String location, String description, String postedDate, int status, String username) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.location = location;
+        this.description = description;
+        this.postedDate = postedDate;
+        this.status = status;
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(String postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
     
 }
